d2ed7fb896865577bf4c16b962bfc398589d6ff3
SHUBHOBRATA ROY <shubho16.2@gmail.com>
2016-08-13 00:24:08 +0600

    Validation bug fixed

--- Projects/Web Version/register/js/validate.js
+++ Projects/Web Version/register/js/validate.js
@@ -1,5 +1,6 @@
 function validateRegistration()
    {
+     var submitform=true;
      //username
      var xhttp = new XMLHttpRequest();
      xhttp.onreadystatechange= function () {
@@ -7,7 +8,7 @@ function validateRegistration()
        {
          document.getElementById('loading2').style.visibility='hidden';
          document.getElementById('usernameAlert').innerHTML = xhttp.responseText;
-         if(xhttp.responseText=='This username is already taken') return false;
+         if(xhttp.responseText=='This username is already taken') submitform=false;
        }
        else document.getElementById('loading2').style.visibility='visible';
      }
@@ -17,15 +18,15 @@ function validateRegistration()
 
      //email
      var email = document.getElementById('email').value;
-     if(email=='') {document.getElementById('emailAlert').innerHTML='Email field is empty'; return false;}
+     if(email=='') {document.getElementById('emailAlert').innerHTML='Email field is empty'; submitform=false;}
 
-     else if (email.indexOf("@")>=email.indexOf(".com")){ document.getElementById('emailAlert').innerHTML='Invalid email format'; return false;}
+     else if (email.indexOf("@")>=email.indexOf(".com")){ document.getElementById('emailAlert').innerHTML='Invalid email format'; submitform=false;}
 
      else document.getElementById('emailAlert').innerHTML='';
 
      //password
      if(document.getElementById('password').value!=document.getElementById('confirm').value)
-     {document.getElementById('passwordAlert').innerHTML='Passwords do not match'; return false;}
+     {document.getElementById('passwordAlert').innerHTML='Passwords do not match'; submitform=false;}
 
      else
        document.getElementById('passwordAlert').innerHTML='';
@@ -34,9 +35,9 @@ function validateRegistration()
      if(document.getElementById('address').value=='')
      {
        document.getElementById('addressAlert').innerHTML='Address Field is empty';
-       return false;
+       submitform=false;
      }
      else document.getElementById('addressAlert').innerHTML='';
 
-     return true;
+     return submitform;
    }
