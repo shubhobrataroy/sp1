51a0b99f72a8d1f252d678e7f28690efcb790dee
SHUBHOBRATA ROY <shubho16.2@gmail.com>
2016-08-13 17:42:55 +0600

    Pending Request can now accept requests

--- Projects/Web Version/admin.php
+++ Projects/Web Version/admin.php
@@ -31,6 +31,22 @@ var refresh=function()
 
 setInterval(refresh,3000);
 
+
+function runAjaxcommand(q,username,pending,accept,reject,password)
+{
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange= function () {
+        if(xhttp.readyState==4)
+        {
+            $('#pendingNumbers').html(xhttp.response);
+        }
+        else document.getElementById('loading2').style.visibility='visible';
+    }
+    var command='ajax.php?q='+q+'&username='+username+'&pending='+pending+'&accept='+accept+'&reject='+reject+'&password='+password;
+    xhttp.open("GET",command,true);
+    xhttp.send();
+}
+
 $(document).ready(function() {
     $('#body').click(function () {
         $('#pendingReq').modal('show');
@@ -48,6 +64,13 @@ $(document).ready(function() {
 
     });
 });
+
+
+    function accept(username,password)
+    {
+        runAjaxcommand('','','',username,'',password);
+        $('#body').click();
+    }
  </script>
 
 
--- Projects/Web Version/ajax.php
+++ Projects/Web Version/ajax.php
@@ -4,7 +4,8 @@ $link = mysql_connect('localhost:3306','root','') or die("Connecton error");
 mysql_select_db("users");
 
 
-if($_GET['q']!='') {
+if($_GET['q']!='') //Username Suggestions are retrived from here
+{
     $res= mysql_query("select username from users.details where username like '".$_GET["q"]."%';") or die("Could not connect to database ");
     if (sizeof($res) == 0) echo 'No suggestions';
 
@@ -16,7 +17,7 @@ if($_GET['q']!='') {
 
     }
 }
-else if($_GET["username"]!='')
+else if($_GET["username"]!='') // Duplicate usernames are checked here
 {
     $res= mysql_query("select username from users.details where username='".$_GET["username"]."';") or die("Could not connect to database ");
     if (sizeof($res) != 0)
@@ -25,16 +26,26 @@ else if($_GET["username"]!='')
              if($row['username']==$_GET['username'])echo 'This username is already taken';
     }}
 }
-else if($_GET['pending']=='true')
+else if($_GET['pending']=='true') //Total pending requests are retrived from here
 {
     $res= mysql_query("select * from users.Registration ;") or die("Could not connect to database ");
     echo mysql_num_rows($res);
 }
 
-else if($_GET['pending']=='info')
+else if($_GET['pending']=='info') //Pending request table is retrived here
 {
     $res= mysql_query("select * from users.Registration ;") or die("Could not connect to database ");
     echo '<table class="table-hover table-bordered" style="width: 100%">';
+
+    echo '<tr>';
+        echo '<td>'.'Username'.'</td>';
+        echo '<td>'.'Email'.'</td>';
+        echo '<td>'.'Password'.'</td>';
+        echo '<td>'.'Address'.'</td>';
+        echo '<td>'.'Accept'.'</td>';
+        echo '<td>'.'Reject'.'</td>';
+    echo '</tr>';
+
     while($row=mysql_fetch_array($res))
     {
         echo '<tr>';
@@ -42,10 +53,22 @@ else if($_GET['pending']=='info')
           echo '<td>'.$row['email'].'</td>';
           echo '<td>'.$row['password'].'</td>';
           echo '<td>'.$row['address'].'</td>';
+          echo '<td>'."<button type='button' class='btn btn-success' onclick=accept('".$row['uname']."','".$row['password']."')>Accept</button>".'</td>';
+          echo '<td>'."<button type='button' class='btn btn-danger' onclick=reject('".$row['uname']."')>Reject</button>".'</td>';
         echo '</tr>';
+
     }
     echo '</table>';
 }
+
+else if($_GET['accept']!='')
+{
+    $query = "DELETE FROM registration WHERE uname='".$_GET['accept']."'";
+    $query2 = "insert into details values ('".$_GET['accept']."','".$_GET['password']."','employee');";
+    $res= mysql_query($query2,$link) ;
+    $res= mysql_query($query) or die('Error 1');
+}
+
 
 
 ?>
