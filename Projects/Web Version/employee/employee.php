76b5dc11ac28742bd57500b389ede09d76c03b7c
diptajustingomes <diptagomes@gmail.com>
2016-08-26 11:09:45 +0600

    Employee Module Edited

--- Projects/Web Version/employee/employee.php
+++ Projects/Web Version/employee/employee.php
@@ -65,7 +65,7 @@ function checkTime(i) {
 
 	 <div class="row btn-block col-md-12 " style="margin: auto;margin-top: 0.3%;">
 			<input type='button' class='buttonRed col-md-2'    value='View Notice Board' id="notice" data-toggle="modal" data-target="#viewnotice" />
-			<input type='button' class='buttonGreen col-md-2'   value='View Task History' id="notice" data-toggle="modal" data-target="#viewtask" />
+			<input type='button' class='buttonGreen col-md-2'   value='View Task History' id="taskhistorybtn" data-toggle="modal" data-target="#viewtask" />
 			<input type='button' class='buttonBlue col-md-2'   value='Download Task Document' id="notice" data-toggle="modal" data-target="#downloadtask" />
 			<input type='button' class='buttonBrownie col-md-2'   value='Employee Profile' id="profilebtn" data-toggle="modal" data-target="#viewprofile" />
 			<input type='button' class='buttonBlueman col-md-2'   value=Time  id="txt1" id="notice" />
@@ -90,7 +90,7 @@ function checkTime(i) {
 		</div>
 
 		<div class='tasks col-md-6 col-sm-6 col-xs-6' id='body4'>
-			<h1  style="color:white;text-align:center;"">0</h1>
+			<h1  style="color:white;text-align:center;" id="tasknumber">0</h1>
 			<h4  style="color:white;text-align:center">Assigned Tasks</h4>  
 		</div>
 	</div>		
@@ -111,14 +111,14 @@ function checkTime(i) {
                 </div>
             </div>
      </div>
-	 <div id="viewtask" class="modal fade" role="dialog">
+	 <div id="viewassignedtask" class="modal fade" role="dialog">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
                         <button type="button" class="close" data-dismiss="modal">&times;</button>
                         <h4 class="modal-title">Modal Header</h4>
                     </div>
-                    <div class="modal-body">
+                    <div class="modal-body" id="assigntask">
                         <span></span>
                     </div>
                     <div class="modal-footer">
